301deda62be6f313e7c841a87ef7d497d14495fa
Darwin Alejandro Elegiga Lopez <darwin.elegiga02@gmail.com>
2023-11-16 17:25:51 -0500

    Agregado muestra de Listado de los pacientes con edad superior a la media

--- script.ts
+++ script.ts
@@ -1,12 +1,12 @@
 class Paciente{
-    private nombre: string;
-    private apellidos: string;
-    private ci: string;
-    private municipio: string;
-    private sexo: boolean;
-    private nc: number;
-    private sala: number;
-    private edad: number;
+        private nombre: string;
+        private apellidos: string;
+        private ci: string;
+        private municipio: string;
+        private sexo: boolean;
+        private nc: number;
+        private sala: number;
+        private edad: number;
 
     constructor(nombre: string, apellidos:string, ci:string,municipio:string, sexo:boolean,nc:number,sala:number, edad:number){
         this.nombre = nombre;
@@ -21,14 +21,16 @@ class Paciente{
     getnombre():string{
         return (this.nombre);
     }
+    
     getsala():number{
         return (this.sala);
     }
+    
     getpaciente():string{
-     return ("Nombre: " + this.nombre +" Apellido: "+ this.apellidos +" CI: " +this.ci + " Municipio: " + this.municipio +" Numero de cama: " + this.nc + " Sala: " + this.sala + " Edad: " + this. edad);   
+        return ("Nombre: " + this.nombre +" Apellido: "+ this.apellidos +" CI: " +this.ci + " Municipio: " + this.municipio +" Numero de cama: " + this.nc + " Sala: " + this.sala + " Edad: " + this. edad);   
     }
     
-        total(a:Paciente[]):string{
+    total(a:Paciente[]):string{
         let contmasc:number=0;
         let contfem:number=0;
 
@@ -44,25 +46,44 @@ class Paciente{
         return "La cantidad de mujeres es: " + contfem + " La cantidad de hombres: " + contmasc;
     }
 
-}
+    paciente_media(a:Paciente[]):string[]{
+        let media:number=0;
+        let x:string[]=[];
+
+        for(let i=0;i<a.length;i++){
+           media+= a[i].edad;
+        }
+        media = media/a.length;
+        console.log("La media es: " + Math.round(media));
+        for(let i=0;i<a.length; i++){
+            if(a[i].edad>media){
+                x.push(a[i].getpaciente());
+            }
+        }
+
+     return x;
+    }
+    
+
+}   
 
 
 let pacientes:Paciente[]=[];
 let p1: Paciente = new Paciente("darwin","elegiga","0210068265","Palma Soriano",true,1,4,22);
-let p2: Paciente = new Paciente("ALejandro","elegiga","0210068265","Palma Soriano",true,1,4,22);
-let p3: Paciente = new Paciente("Saiz","elegiga","0210068265","Palma Soriano",false,1,4,22);
+let p2: Paciente = new Paciente("ALejandro","elegiga","0210068265","Palma Soriano",true,1,4,19);
+let p3: Paciente = new Paciente("Saiz","elegiga","0210068265","Palma Soriano",false,1,4,24);
 
-let p4: Paciente = new Paciente("Ricardo","elegiga","0210068265","Palma Soriano",false,2,3,22);
-let p5: Paciente = new Paciente("Roberto","elegiga","0210068265","Palma Soriano",false,2,3,22);
-let p6: Paciente = new Paciente("Raul","elegiga","0210068265","Palma Soriano",false,2,3,22);
+let p4: Paciente = new Paciente("Ricardo","elegiga","0210068265","Palma Soriano",false,2,3,60);
+let p5: Paciente = new Paciente("Roberto","elegiga","0210068265","Palma Soriano",false,2,3,70);
+let p6: Paciente = new Paciente("Raul","elegiga","0210068265","Palma Soriano",false,2,3,14);
 
-let p7: Paciente = new Paciente("Fernando","elegiga","0210068265","Palma Soriano",true,3,2,22);
-let p8: Paciente = new Paciente("Zambia","elegiga","0210068265","Palma Soriano",false,3,2,22);
-let p9: Paciente = new Paciente("Reinier","elegiga","0210068265","Palma Soriano",true,3,2,22);
+let p7: Paciente = new Paciente("Fernando","elegiga","0210068265","Palma Soriano",true,3,2,19);
+let p8: Paciente = new Paciente("Zambia","elegiga","0210068265","Palma Soriano",false,3,2,20);
+let p9: Paciente = new Paciente("Reinier","elegiga","0210068265","Palma Soriano",true,3,2,21);
 
-let p10: Paciente = new Paciente("Luis","elegiga","0210068265","Palma Soriano",true,4,1,22);
-let p11: Paciente = new Paciente("Andres","elegiga","0210068265","Palma Soriano",true,4,1,22);
-let p12: Paciente = new Paciente("Salvador","elegiga","0210068265","Palma Soriano",true,4,1,22);
+let p10: Paciente = new Paciente("Luis","elegiga","0210068265","Palma Soriano",true,4,1,40);
+let p11: Paciente = new Paciente("Andres","elegiga","0210068265","Palma Soriano",true,4,1,50);
+let p12: Paciente = new Paciente("Salvador","elegiga","0210068265","Palma Soriano",true,4,1,33);
 
 pacientes.push(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12);
 
@@ -105,3 +126,9 @@ console.log("Sala #4: "+sala4)
 console.log("-----")
 
 console.log(pacientes[0].total(pacientes));
+
+
+/*mostrando media*/
+let x:string[]=pacientes[0].paciente_media(pacientes);
+for(let i=0; i<x.length;i++){
+console.log("La media es:" + x[i] );}
