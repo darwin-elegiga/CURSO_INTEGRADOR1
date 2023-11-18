ee70ee92efb664cf3ded3c693979567bdbc201c4
Darwin Alejandro Elegiga Lopez <darwin.elegiga02@gmail.com>
2023-11-18 12:50:55 -0500

    Mejorado script.js y css

--- scriptvisual.js
+++ scriptvisual.js
@@ -2,12 +2,19 @@
 function execute(){
     var selector = document.querySelector(".funcionalidad_select");
     const opciones = document.querySelector(".opciones");
+    const buscarCI = document.querySelector(".buscarCI");
 
+    console.log(selector.value)
     if(selector.value!=null){
         opciones.classList.add("opciones-activa");
         selector.classList.add("funcionalidad_select-activa");
 
     }; 
 
-     
+    if(selector.value=="pxci"){
+        buscarCI.classList.toggle("buscarCI-active");
+    }; 
+    if(selector.value!="pxci"){
+        buscarCI.classList.remove("buscarCI-active");
+    }; 
 }
